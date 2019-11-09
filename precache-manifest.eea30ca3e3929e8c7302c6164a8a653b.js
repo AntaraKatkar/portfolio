20223a82499be791b2876e13c54541d0ed9e3bf2
AntaraKatkar <gitalirakshak007@gmail.com>
2019-11-08 19:16:06 -0600

    Updates

---
+++ precache-manifest.eea30ca3e3929e8c7302c6164a8a653b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b523dd45903f5f757a8da3b4950fa44",
+    "url": "/AntaraKatkar/portfolio/index.html"
+  },
+  {
+    "revision": "186b6d651441b699ba08",
+    "url": "/AntaraKatkar/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/AntaraKatkar/portfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "186b6d651441b699ba08",
+    "url": "/AntaraKatkar/portfolio/static/js/main.e929ff4c.chunk.js"
+  },
+  {
+    "revision": "7a077f72495443f52a12",
+    "url": "/AntaraKatkar/portfolio/static/js/runtime~main.77b78c36.js"
+  }
+]);
