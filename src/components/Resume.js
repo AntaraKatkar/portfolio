9b6f3a56818c364c68c90f4233b5cf916ae89d93
AntaraKatkar <55859942+AntaraKatkar@users.noreply.github.com>
2019-12-04 19:39:12 -0600

    Update Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,7 +4,7 @@ import "../App.css";
 
 //For File viewer
 import FileViewer from 'react-file-viewer';
-const file = 'https://AntaraKatkar.github.io/portfolio/public/resume/ResumeGitaliRakshak.docx';
+const file = '../portfolio/public/resume/ResumeGitaliRakshak.docx';
 const type = 'docx';
 
 class Resume extends Component {
