9594daaa45354080c80648314f078e6c85793e81
AntaraKatkar <gitalirakshak007@gmail.com>
2019-12-04 19:17:33 -0600

    Updates

---
+++ precache-manifest.ba585932fd22ed8422e730414fa94302.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ebb44da9be1a54f2d7533e52e346393",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "42087cfb8a31b9af1219",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9696e0c6ab25e9b54d8f",
+    "url": "/portfolio/static/js/2.54b90ead.chunk.js"
+  },
+  {
+    "revision": "42087cfb8a31b9af1219",
+    "url": "/portfolio/static/js/main.4762ab66.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f1c51fd815abc26ef76e726504ee09c3.js"
+  "/portfolio/precache-manifest.ba585932fd22ed8422e730414fa94302.js"
 );
 
 self.addEventListener('message', (event) => {
