932a43924e069e553a82b09aa910570d03b22d36
AntaraKatkar <gitalirakshak007@gmail.com>
2019-12-06 14:35:17 -0600

    Resume section added

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,16 +4,9 @@ import "../App.css";
 //import { Document, Page, Text, View, StyleSheet } from '@react-pdf/renderer';
 
 //For File viewer
-<<<<<<< HEAD
-//import FileViewer from 'react-file-viewer';
-const file = '../resume/ResumeGitaliRakshak.docx';
-//const file = 'https://AntaraKatkar.github.io/portfolio/public/resume/ResumeGitaliRakshak.docx';
-const file = 'https://AntaraKatkar.github.io/portfolio/resume/ResumeGitaliRakshak.docx';
-=======
 import FileViewer from 'react-file-viewer';
-const file = 'https://AntaraKatkar.github.io/portfolio/resume/ResumeGitaliRakshak.docx';
+const file = "https://AntaraKatkar.github.io/portfolio/resume/ResumeGitaliRakshak.docx";
 //const file ='https://github.com/AntaraKatkar/portfolio/blob/master/public/resume/ResumeGitaliRakshak.docx';
->>>>>>> 5c41e63785cd236e8b59a343d207891fd263d9d9
 const type = 'docx';
 
 class Resume extends Component {
@@ -25,7 +18,7 @@ class Resume extends Component {
 
         <FileViewer
         fileType={type}
-        filePath={file}
+        filePath={'https://AntaraKatkar.github.io/portfolio/resume/ResumeGitaliRakshak.docx'}
         //errorComponent={ CustomErrorComponent }
         onError={this.onError}/>
 
